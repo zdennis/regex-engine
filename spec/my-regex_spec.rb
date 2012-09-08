66c63f5e84a1e811f7d714109d2bc582ff6b0b0e
Zach Dennis <zach.dennis@gmail.com>
2012-09-08 12:25:12 -0400

    Refactoring the spec to use should_match and should_not_match macros.

--- spec/my-regex_spec.rb
+++ spec/my-regex_spec.rb
@@ -3,58 +3,65 @@ require 'spec_helper'
 describe "regex matching" do
   subject { regex_engine }
   let(:regex_engine) { MyRegex.new(pattern) }
+  let(:pattern){ example.example_group.description }
+
+  def self.should_match(str)
+    it "matches #{str.inspect}" do
+      should have_match(str)
+    end
+  end
+
+  def self.should_not_match(str)
+    it "doesn't match #{str.inspect}" do
+      should_not have_match(str)
+    end
+  end
 
   describe "/a/" do
-    let(:pattern){ "/a/" }
-    it { should have_match("a") }
-
-    it { should_not have_match("b") }
+    should_match "a"
+    should_not_match "b"
   end
 
   describe "/abc/" do
-    let(:pattern){ "/abc/" }
-    it { should have_match("abc") }
-    it { should have_match("zabc") }
+    should_match "abc"
+    should_match "zabc"
 
-    it { should_not have_match("bca") }
-    it { should_not have_match("cba") }
-    it { should_not have_match("ab.") }
+    should_not_match "bca"
+    should_not_match "cba"
+    should_not_match "ab."
   end
 
   describe "/a.c/" do
-    let(:pattern){ "/a.c/" }
-    it { should have_match("abc") }
-    it { should have_match("zabc") }
-    it { should have_match("zadcasdf") }
+    should_match "abc"
+    should_match "zabc"
+    should_match "zadcasdf"
 
-    it { should_not have_match("cbc") }
-    it { should_not have_match("zabd") }
-    it { should_not have_match("zadbca") }
+    should_not_match "cbc"
+    should_not_match "zabd"
+    should_not_match "zadbca"
   end
 
   describe "0 or more occurrences" do
     describe "/a.*c/" do
-      let(:pattern){ "/a.*c/" }
-      it { should have_match("ac") }
-      it { should have_match("abc") }
-      it { should have_match("abbc") }
-      it { should have_match("zabdddddc") }
-      it { should have_match("zadcasdf") }
-      it { should have_match("zacasdfacg") }
+      should_match "ac"
+      should_match "abc"
+      should_match "abbc"
+      should_match "zabdddddc"
+      should_match "zadcasdf"
+      should_match "zacasdfacg"
 
-      it { should_not have_match("abddddd") }
+      should_not_match "abddddd"
     end
 
     describe "/ab*c/" do
-      let(:pattern){ "/ab*c/" }
-      it { should have_match("abc") }
-      it { should have_match("abbbbbbbbbbbbc") }
-      it { should have_match("zacasdfacg") }
+      should_match "abc"
+      should_match "abbbbbbbbbbbbc"
+      should_match "zacasdfacg"
 
-      it { should have_match("abbc") }
-      it { should_not have_match("zabdddddc") }
-      it { should_not have_match("zadcasdf") }
-      it { should_not have_match("abddddd") }
+      should_match "abbc"
+      should_not_match "zabdddddc"
+      should_not_match "zadcasdf"
+      should_not_match "abddddd"
     end
 
   end
