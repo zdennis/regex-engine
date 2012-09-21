79a147877e6c1f645e6314065d4e0b82def2c43c
Zach Dennis <zach.dennis@gmail.com>
2012-09-20 21:42:50 -0400

    Refactoring: removing Acceptor class and pushing responsibility into MyRegex and simplifying the method that compiles the pattern initially.

--- lib/my-regex.rb
+++ lib/my-regex.rb
@@ -1,110 +1,101 @@
 class MyRegex
   def initialize(pattern)
-    # Only accept regexes in form of /../
-    # so strip out the forward slashes at beginning
-    # and end of the pattern
+    @acceptors = []
+
+    # strip out leading/trailing slashes
     @pattern = pattern[1..-2]
 
-    @engine = Acceptor.new(@pattern)
+    compile @pattern
   end
 
   def =~(str)
-    if md=@engine.match(str)
+    if md=match(str)
       md.offset
     end
   end
 
-  class Acceptor
-    def initialize(pattern)
-      pindex = 0
-      @acceptors = []
+  private
 
-      loop do
-        current_ch = pattern[pindex]
-        next_ch    = pattern[pindex+1]
-        puts "START: #{pattern[pindex].inspect}  #{current_ch.inspect}" if ENV["DEBUG"]
+  def compile(pattern)
+    pindex = 0
 
-        if acceptor_klass=wrap_previous_acceptor_map[current_ch]
-          prev_acceptor = @acceptors.last
-          @acceptors[-1] = acceptor_klass.new(prev_acceptor)
+    while pattern.length > 0 && pindex < pattern.length
+      char = pattern[pindex]
 
-          next_index = pindex + 1          
-          pattern = pattern[next_index..-1]
-          pindex  = 0
-        elsif acceptor_klass=add_acceptor_map[current_ch]
-          @acceptors << acceptor_klass.new(current_ch)
-
-          next_index = pindex + 1          
-          pattern = pattern[next_index..-1]
-          pindex  = 0
-        end
-
-        break if pattern.length == 0 || (pindex == pattern.length)
+      if acceptor_klass=wrap_previous_acceptor_map[char]
+        prev_acceptor = @acceptors.last
+        @acceptors[-1] = acceptor_klass.new(prev_acceptor)
+      elsif acceptor_klass=add_acceptor_map[char]
+        @acceptors << acceptor_klass.new(char)
       end
+
+      next_index = pindex + 1          
+      pattern = pattern[next_index..-1]
+      pindex  = 0
     end
+  end
 
-    def add_acceptor_map
-      Hash.new(SingleCharacterAcceptor).merge("." => AnyCharacterAcceptor)
-    end
+  def add_acceptor_map
+    @add_acceptor_map ||= Hash.new(SingleCharacterAcceptor).merge("." => AnyCharacterAcceptor)
+  end
 
-    def wrap_previous_acceptor_map
-      {
-        "?" => ZeroOrOneAcceptor,      
-        "*" => ZeroOrMoreAcceptor,
-        "+" => OneOrMoreAcceptor
-      }
-    end
+  def wrap_previous_acceptor_map
+    @wrap_previous_acceptor_map ||= {
+      "?" => ZeroOrOneAcceptor, 
+      "*" => ZeroOrMoreAcceptor,
+      "+" => OneOrMoreAcceptor 
+    }
+  end
 
-    def match(str)
-      str4match = str.dup
-      acceptor_stack = @acceptors.dup
-      accepted_stack = []
-      sindex = 0
-      offset = 0
+  def match(str)
+    str4match = str.dup
+    acceptor_stack = @acceptors.dup
+    accepted_stack = []
+    sindex = 0
+    offset = 0
 
-      tried_states = []
+    tried_states = []
 
-      loop do
-        acceptor = acceptor_stack.first
-        str4match = str[sindex..-1]
-        puts "str4match: #{str4match.inspect} #{acceptor.inspect}" if ENV["DEBUG"]
+    loop do
+      acceptor = acceptor_stack.first
+      str4match = str[sindex..-1]
+      puts "str4match: #{str4match.inspect} #{acceptor.inspect}" if ENV["DEBUG"]
 
-        possible_match_state = [sindex, acceptor, (acceptor && acceptor.retry_length)]
+      possible_match_state = [sindex, acceptor, (acceptor && acceptor.retry_length)]
 
-        if accepted_stack.length == 0
-          offset = sindex
-        end
+      if accepted_stack.length == 0
+        offset = sindex
+      end
 
-        if str4match.nil? || tried_states.include?(possible_match_state)
-          return false 
-        end
+      if str4match.nil? || tried_states.include?(possible_match_state)
+        return false 
+      end
+      tried_states << possible_match_state
+
+      if acceptor.match(str4match, acceptor.retry_length)
+        puts "  matched at #{sindex}, #{acceptor.matched_length}" if ENV["DEBUG"]
         tried_states << possible_match_state
+        sindex += acceptor.matched_length
+        accepted_stack.push acceptor
+        acceptor_stack = acceptor_stack[1..-1]
+      elsif accepted_stack.empty?
+        puts "  not matched (stack empty, move forward one character)" if ENV["DEBUG"]
+        sindex += 1
+      else
+        puts "  not matched resetting" if ENV["DEBUG"] 
+        acceptor_stack = [accepted_stack.pop].concat(acceptor_stack)
+        accepted_stack = accepted_stack #[0..-2] || []
 
-        if acceptor.match(str4match, acceptor.retry_length)
-          puts "  matched at #{sindex}, #{acceptor.matched_length}" if ENV["DEBUG"]
-          tried_states << possible_match_state
-          sindex += acceptor.matched_length
-          accepted_stack.push acceptor
-          acceptor_stack = acceptor_stack[1..-1]
-        elsif accepted_stack.empty?
-          puts "  not matched (stack empty, move forward one character)" if ENV["DEBUG"]
-          sindex += 1
-        else
-          puts "  not matched resetting" if ENV["DEBUG"] 
-          acceptor_stack = [accepted_stack.pop].concat(acceptor_stack)
-          accepted_stack = accepted_stack #[0..-2] || []
+        puts "  try again: #{sindex} to #{sindex - acceptor_stack.first.matched_length}" if ENV["DEBUG"]
+        sindex -= acceptor_stack.first.matched_length
+      end
 
-          puts "  try again: #{sindex} to #{sindex - acceptor_stack.first.matched_length}" if ENV["DEBUG"]
-          sindex -= acceptor_stack.first.matched_length
-        end
+      puts "sindex (#{sindex} of #{str.length})" if ENV["DEBUG"]
 
-        puts "sindex (#{sindex} of #{str.length})" if ENV["DEBUG"]
-
-        if acceptor_stack.empty?
-          return MatchData.new(offset)
-        elsif str4match.nil? || sindex > str.length
-          return nil
-        end
+      if acceptor_stack.empty?
+        return MatchData.new(offset)
+      elsif str4match.nil? || sindex > str.length
+        return nil
       end
     end
   end
